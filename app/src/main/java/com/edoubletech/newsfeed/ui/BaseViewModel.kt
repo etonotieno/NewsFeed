4af148cceaf0c8035d6a515d5fb9f27e3cc07447
Eton Otieno <etonotieno@gmail.com>
2018-10-31 15:52:28 +0300

    Create a BaseViewModel that contains the scope to be used by all ViewModels

---
+++ app/src/main/java/com/edoubletech/newsfeed/ui/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.edoubletech.newsfeed.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+open class BaseViewModel : ViewModel() {
+
+    private val baseJob = Job()
+    protected val uiScope = CoroutineScope(Dispatchers.Main + baseJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        baseJob.cancel()
+    }
+}
