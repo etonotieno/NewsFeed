d7e58638f95277a9c35ce6ac6cf7d442a57b8fa8
Eton Otieno <etonotieno@gmail.com>
2023-05-16 18:54:40 +0300

    Set up Compose NewsFeed theme

---
+++ app/src/main/java/io/devbits/newsfeed/ui/settings/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package io.devbits.newsfeed.ui.settings.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryColor = Color(0xFFDD352E)
+val primaryDarkColor = Color(0xFF83251D)
+val colorOnPrimary = Color(0xFFF4EEED)
+
+val secondaryColor = Color(0xFFF17801)
+val secondaryDarkColor = Color(0xF2A04B03)
+val colorOnSecondary = Color(0xFFF4EEED)
+
+val tertiaryColor = Color(0xFF61B872)
+
+val colorPrimary = Color(0xFFDD352E)
+val colorPrimaryVariant = Color(0xFF83251D)
+val colorSecondary = Color(0xFFF17801)
+val colorSecondaryVariant = Color(0xF2A04B03)
+
+val colorSurface = Color(0xFFFFFFFF)
