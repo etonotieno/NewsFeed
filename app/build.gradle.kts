d95ab5de9603d0daee436725acdc3ef9231a4663
Eton Otieno <etonotieno@gmail.com>
2023-08-07 15:47:19 +0300

    Update build files to kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,50 +13,52 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import java.util.Properties
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-parcelize")
     id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("androidx.navigation.safeargs.kotlin")
 }
 
-def getProperties(String fileName) {
-    final Properties properties = new Properties()
-    def file = file(fileName)
+fun getPropertiesFile(fileName: String): Properties {
+    val properties = Properties()
+    val file = File(fileName)
     if (file.exists()) {
-        file.withInputStream { stream -> properties.load(stream) }
+        file.inputStream().use { stream -> properties.load(stream) }
     }
     return properties
 }
 
-static def getFromProperties(Properties properties, String name) {
+fun getProperty(properties: Properties, name: String): String {
     return properties.getProperty(name) ?: "$name missing"
 }
 
-def SECRETS_FILE = getProperties(rootProject.file("secrets.properties").path)
-def GUARDIAN_API_KEY = getFromProperties(SECRETS_FILE, "GUARDIAN_API_KEY")
-def NEWS_API_KEY = getFromProperties(SECRETS_FILE, "NEWS_API_KEY")
+val properties = getPropertiesFile("$rootDir/secrets.properties")
+val guardianApiKey = getProperty(properties, "GUARDIAN_API_KEY")
+val newsApiKey = getProperty(properties, "NEWS_API_KEY")
 
 android {
-    compileSdkVersion = 33
+    compileSdk = 33
     namespace = "io.devbits.newsfeed"
 
     defaultConfig {
         applicationId = "io.devbits.newsfeed"
-        minSdkVersion(21)
-        targetSdkVersion(33)
+        minSdk = 21
+        targetSdk = 33
         versionCode = 1
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
-        buildConfigField("String", "GUARDIAN_API_KEY", GUARDIAN_API_KEY)
-        buildConfigField("String", "NEWS_API_KEY", NEWS_API_KEY)
+        buildConfigField("String", "GUARDIAN_API_KEY", guardianApiKey)
+        buildConfigField("String", "NEWS_API_KEY", newsApiKey)
     }
     buildTypes {
         release {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
@@ -73,35 +75,45 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes.apply {
+                add("META-INF/LICENSE")
+                add("META-INF/LICENSE-notice.md")
+                add("META-INF/LICENSE.md")
+                add("META-INF/*.properties")
+                add("META-INF/AL2.0")
+                add("META-INF/LGPL2.1")
+            }
         }
     }
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.7.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.7")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
 
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
 
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
 
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation("androidx.activity:activity-compose:1.7.2")
+
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -111,42 +123,42 @@ dependencies {
 
     implementation("com.google.android.material:material:1.9.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    implementation("com.google.dagger:dagger:2.45")
-    kapt("com.google.dagger:dagger-compiler:2.45")
+    implementation("com.google.dagger:dagger:2.47")
+    kapt("com.google.dagger:dagger-compiler:2.47")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
 
-    implementation("androidx.room:room-runtime:2.5.1")
-    implementation("androidx.room:room-ktx:2.5.1")
-    implementation("androidx.room:room-paging:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("androidx.room:room-paging:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
 
-    implementation("androidx.viewpager2:viewpager2:1.1.0-beta01")
+    implementation("androidx.viewpager2:viewpager2:1.1.0-beta02")
 
-    implementation("com.github.bumptech.glide:glide:4.13.2")
-    kapt("com.github.bumptech.glide:compiler:4.13.2")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    ksp("com.github.bumptech.glide:ksp:4.14.2")
 
-    implementation("io.coil-kt:coil:2.3.0")
-    implementation("io.coil-kt:coil-compose:2.3.0")
+    implementation("io.coil-kt:coil:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 
     implementation("net.danlew:android.joda:2.12.5")
 
-    implementation("com.squareup.leakcanary:plumber-android:2.10")
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
+    implementation("com.squareup.leakcanary:plumber-android:2.12")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
 }
